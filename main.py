f4c9f7ca1ea44329d4cf1af0aef2a948ef723faa
Gual Escalante <gual23@gmail.com>
2018-09-17 17:58:53 -0600

    Added script control options, added procedure code cleanup

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-print("Hello!")
+import csv
+
+options = {1: 'Procedures'}
+
+print('Welcome to the OMR procedure import process!')
+print('Current options:')
+
+for key, value in options.items():
+  print('%d) %s' % (key, value))
+
+selected_option = int(input('Please enter your selection:'))
+
+if selected_option in options:
+  if selected_option == 1:
+    with open('procedures.csv', encoding='iso-8859-1') as csvfile:
+      reader = csv.DictReader(csvfile)
+      for row in reader:
+        code = row['code'].replace(' ', '')
+        print(code)
+else:
+  input('Option not implemented, bye.')
+  exit()
