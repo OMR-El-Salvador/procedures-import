dc5dea3ddbaafdde1443f63e4bac6f387a12297c
Gual Escalante <gual23@gmail.com>
2018-09-20 16:30:07 -0600

    Connect to PG and procedure insert

--- main.py
+++ main.py
@@ -14,12 +14,21 @@ for key, value in options.items():
 selected_option = int(input('Please enter your selection:'))
 
 if selected_option in options:
+  import db
+
   if selected_option == 1:
     with open('procedures.csv', encoding='iso-8859-1') as csvfile:
       reader = csv.DictReader(csvfile)
       for row in reader:
         code = row['code'].replace(' ', '')
-        print(code)
+        name = row['name']
+        desc = row['description']
+        qs = 'INSERT INTO api.procedures(code, name, description) VALUES (%s, %s, %s)'
+        values = (code, name, desc)
+        db.create_record(qs, values)
+        print('Imported: ' + code)
+
+  db.complete_operations()
 else:
   print('Option not implemented, bye.')
   exit()
