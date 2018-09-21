6915ec2d85cbe1f66a54449426057e1fd882a58c
Gual Escalante <gual23@gmail.com>
2018-09-21 15:51:38 -0600

    Added printing # of affected rows

--- entities/db.py
+++ entities/db.py
@@ -2,6 +2,7 @@ import psycopg2
 
 class DB():
   conn = None
+  qty = 0
 
   def __init__(self):
     self.conn = psycopg2.connect(
@@ -13,11 +14,13 @@ class DB():
   def create_record(self, qs, values):
     cur = self.conn.cursor()
     cur.execute(qs, values)
+    self.qty += 1
 
   def empty_table(self, table):
     cur = self.conn.cursor()
     cur.execute('DELETE FROM ' + table + ';')
 
   def complete_operations(self):
+    print(str(self.qty) + ' rows affected.')
     self.conn.commit()
     self.conn.close()
